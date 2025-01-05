f184aaa81acd8e891d79e38cf34e919b2f480760
System Administrator <root@Evans-MacBook-Pro.local>
2025-01-05 16:08:01 -0500

    New init

--- Evan_Coin/__init__.py
+++ Evan_Coin/__init__.py
@@ -2,3 +2,5 @@ from Evan_Coin.block import Block
 from Evan_Coin.transaction import Transaction
 from Evan_Coin.wallet import Wallet
 from Evan_Coin.blockchain import Blockchain
+from Evan_Coin.server import Server
+from Evan_Coin.client import Client
